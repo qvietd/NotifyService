61341777420a9f1b91003fe7aebbbf600132ad8b
qvietd <qvietd@gmail.com>
2025-09-11 08:33:43 +0700

    #update code

--- NotifyService/src/NotifyService.Application/DependencyInjection.cs
+++ NotifyService/src/NotifyService.Application/DependencyInjection.cs
@@ -7,9 +7,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddSingleton<IRabbitMqService, RabbitMQService>();
-        services.AddSingleton<IConnectionMappingService, ConnectionMappingService>();
-        services.AddSingleton<IBatchProcessor, BatchProcessor>();
+        //services.AddSingleton<IRabbitMqService, RabbitMQService>();
+        //services.AddSingleton<IConnectionMappingService, ConnectionMappingService>();
+        //services.AddSingleton<IBatchProcessor, BatchProcessor>();
 
         return services;
     }
